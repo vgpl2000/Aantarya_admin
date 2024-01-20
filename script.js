6c0593d11020216c59412e4bdc4e97fc94970b61
Venugopal <venugpl2000@gmail.com>
2024-01-20 19:07:04 +0530

    Added Password Protection

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+document.getElementById("pass-txt").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      const enteredPassword = event.target.value;
+      const bodyele = document.getElementById("body-id");
+      const passdiv = document.getElementById("pass-div");
+
+      if ("SURvbnRLbm93VGhlUGFzc3dvcmQ=" === btoa(enteredPassword)) {
+        bodyele.style.display="block";
+        passdiv.style.display="none";
+      } else {
+        alert("Incorrect password. Access denied.");
+      }
+    }
+  });
