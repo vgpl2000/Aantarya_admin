1bd96b2ac7b4b5c80906439816a0ad6402e057b2
Venugopal <venugpl2000@gmail.com>
2024-01-20 20:16:56 +0530

    More details added

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 
-
-
-
-document.getElementById("pass-txt").addEventListener("keydown", function(event) {
+document.getElementById("pass-txt").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-      const enteredPassword = event.target.value;
-      const bodyele = document.getElementById("body-id");
-      const passdiv = document.getElementById("pass-div");
+        const enteredPassword = event.target.value;
+        const bodyele = document.getElementById("body-id");
+        const passdiv = document.getElementById("pass-div");
 
-      if ("SURvbnRLbm93VGhlUGFzc3dvcmQ=" === btoa(enteredPassword)) {
-        bodyele.style.display="block";
-        passdiv.style.display="none";
-      } else {
-        alert("Incorrect password. Access denied.");
-      }
+        if ("SURvbnRLbm93VGhlUGFzc3dvcmQ=" === btoa(enteredPassword)) {
+            bodyele.style.display = "block";
+            passdiv.style.display = "none";
+        } else {
+            alert("Incorrect password. Access denied.");
+        }
     }
-  });
+});
