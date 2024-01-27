af3b5e19d04da7f85ebd1395a517adc5aa373669
Akash C Bhat <akashcbhat2001@gmail.com>
2024-01-27 20:02:01 +0530

    data loading

--- dataloader.js
+++ dataloader.js
@@ -7,6 +7,66 @@ const teamNameInput = document.getElementById("team-name");
 const collegeNameInput = document.getElementById("clg-name");
 const categoryInput = document.getElementById("category-input");
 
+const paymentImg = document.getElementById("screenshot");
+const transId = document.getElementById("tran-id");
+const transChk = document.getElementById("pay-check-id");
+
+const accommocheck = document.getElementById("accommo-check");
+const accommonb = document.getElementById("accommo-no-b");
+const accommong = document.getElementById("accommo-no-g");
+
+
+
+const codingMem1 = document.getElementById('coding-mem1');
+const codingMem1Con = document.getElementById('coding-mem1-con');
+const codingMem2 = document.getElementById('coding-mem2');
+const codingMem2Con = document.getElementById('coding-mem2-con');
+const webMem1 = document.getElementById('web-mem1');
+const webMem1Con = document.getElementById('web-mem1-con');
+const webMem2 = document.getElementById('web-mem2');
+const webMem2Con = document.getElementById('web-mem2-con');
+const quizMem1 = document.getElementById('quiz-mem1');
+const quizMem1Con = document.getElementById('quiz-mem1-con');
+const quizMem2 = document.getElementById('quiz-mem2');
+const quizMem2Con = document.getElementById('quiz-mem2-con');
+const debateMem1 = document.getElementById('debate-mem1');
+const debateMem1Con = document.getElementById('debate-mem1-con');
+const danceMem1 = document.getElementById('dance-mem1');
+const danceMem1Con = document.getElementById('dance-mem1-con');
+const danceMem2 = document.getElementById('dance-mem2');
+const danceMem2Con = document.getElementById('dance-mem2-con');
+const danceMem3 = document.getElementById('dance-mem3');
+const danceMem3Con = document.getElementById('dance-mem3-con');
+const danceMem4 = document.getElementById('dance-mem4');
+const danceMem4Con = document.getElementById('dance-mem4-con');
+const danceMem5 = document.getElementById('dance-mem5');
+const danceMem5Con = document.getElementById('dance-mem5-con');
+const danceMem6 = document.getElementById('dance-mem6');
+const danceMem6Con = document.getElementById('dance-mem6-con');
+const danceMem7 = document.getElementById('dance-mem7');
+const danceMem7Con = document.getElementById('dance-mem7-con');
+const photoMem1 = document.getElementById('photo-mem1');
+const photoMem1Con = document.getElementById('photo-mem1-con');
+const gameMem1 = document.getElementById('game-mem1');
+const gameMem1Con = document.getElementById('game-mem1-con');
+const gameMem2 = document.getElementById('game-mem2');
+const gameMem2Con = document.getElementById('game-mem2-con');
+const thuntMem1 = document.getElementById('thunt-mem1');
+const thuntMem1Con = document.getElementById('thunt-mem1-con');
+const thuntMem2 = document.getElementById('thunt-mem2');
+const thuntMem2Con = document.getElementById('thunt-mem2-con');
+const productMem1 = document.getElementById('product-mem1');
+const productMem1Con = document.getElementById('product-mem1-con');
+const itmanMem1 = document.getElementById('itman-mem1');
+const itmanMem1Con = document.getElementById('itman-mem1-con');
+const desigMem1 = document.getElementById('desig-mem1');
+const desigMem1Con = document.getElementById('desig-mem1-con');
+const dumbMem1 = document.getElementById('dumb-mem1');
+const dumbMem1Con = document.getElementById('dumb-mem1-con');
+const dumbMem2 = document.getElementById('dumb-mem2');
+const dumbMem2Con = document.getElementById('dumb-mem2-con');
+
+
 const fetchTeams = async () => {
     try {
         const response = await fetch(`${API_URL}/admin/get-ids`);
@@ -34,7 +94,8 @@ const displayTeams = (teams) => {
 teamList.addEventListener('click', (event) => {
     const clickedTeamId = event.target.dataset.teamId;
     teamDetails(clickedTeamId);
-    console.log('Clicked Team ID:', clickedTeamId);
+    teamPayment(clickedTeamId);
+    accommoNo(clickedTeamId);
 });
 
 
@@ -45,17 +106,61 @@ const teamDetails = async (teamId) => {
     const res = await fetch(`${API_URL}/admin/get-ids/${teamId}`);
     const data = await res.json();
 
-    console.log(data);
     teamNameInput.value = data.teamName;
     collegeNameInput.value = data.collegeName;
-    console.log(data.isUG);
-    console.log(typeof data.isUG);
+    
     categoryInput.value = data.isUG ? 'UG' : 'PG';
 
 
 };
 
 
-
-
 fetchTeams();
+
+
+// payment
+
+
+const teamPayment = async (teamId) => {
+
+    const res = await fetch(`${API_URL}/team/${teamId}`);
+    const data = await res.json();
+    
+    paymentImg.src = data.paymentStatus.screenshot;
+    transId.value = data.paymentStatus.transactionId;
+    transChk.checked = data.paymentStatus.verificationStatus ? true : false ;
+};
+
+
+// accommodation
+
+const accommoNo = async (teamId) => {
+
+    const res = await fetch(`${API_URL}/team/${teamId}`);
+    const data = await res.json();
+    
+    if(data.accommodation !== undefined){
+        accommocheck.value = 'Yes';
+        accommonb.value= data.accommodation.countOfBoys;
+        accommong.value= data.accommodation.countOfGirls;
+    }
+    else{
+        accommocheck.value = 'No';
+        accommonb.value= "0";
+        accommong.value= "0";
+    }
+};
+
+
+// events
+const events = async (teamId) => {
+
+    const res = await fetch(`${API_URL}/team/${teamId}/events`);
+    const data = await res.json();
+
+    
+    
+
+
+    
+};
